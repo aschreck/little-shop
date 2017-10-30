5c5805c593e5b784427f5b103fa4bb4b84f3578e
randy-darsh <rspringer47@gmail.com>
2017-10-30 16:38:12 -0600

    first 2 stories passing

---
+++ spec/features/visitor_can_view_items_by_category_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Visitor visits a categories show page' do
+  it "they see all items associated with that category" do
+    category = Category.create(title: "Stuff")
+    item = category.items.create(title: "Thing", description: "Its a thing", price: 10.00, image: "thing.png")
+
+    visit "/Stuff"
+
+    expect(page).to have_content(category.title)
+    expect(page).to have_content(item.title)
+    expect(page).to have_content(item.description)
+    expect(page).to have_content(item.price)
+  end
+end
