d7de11189dc45300ff6070527132a0a01462e26c
randy-darsh <rspringer47@gmail.com>
2017-10-31 10:44:39 -0600

    add carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,16 +1,20 @@
 class CartsController < ApplicationController
 
 	def create
-
-	end 
+    id = params["item_id"].to_s
+    @item = Item.find_by(id: id)
+    session[:cart] ||= Hash.new(0)
+    session[:cart][id] = (session[:cart][id] || 0) + 1
+    redirect_to items_path
+  end
 
 	def show
-
+    @cart = Cart.new(session[:cart])
 	end 
 
 	def update
 
-	end 
+	end
 
 	
 end
