c724b81e586832aa30c2da55835627a9b4cfe748
randy-darsh <rspringer47@gmail.com>
2017-10-31 11:55:59 -0600

    add route for deleting item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-	resource :cart, only: [:show, :create]
+  resource :cart, only: [:show, :create, :destroy]
   get "/:title", to: "category#show"
 end
