27fd38916e91f6abb05d962de01c75b67ad0f6eb
Aaron Schreck <schreck.aaron@gmail.com>
2017-10-31 13:49:41 -0600

    Cart displays total cost of cart

--- spec/features/user_can_adjust_card_item_quanitity_spec.rb
+++ spec/features/user_can_adjust_card_item_quanitity_spec.rb
@@ -1,7 +1,7 @@
 describe "User visits /cart with items in the cart." do
 	it "they can increase the quantity" do
 	  category = Category.create(title: "Breakfast")
-    item = category.items.create(title: "Soggy Cereal", description: "Kinda gross", price: 1.25, image: "asdfs")
+    item = category.items.create(title: "Soggy Cereal", description: "Kinda gross", price: 1.50, image: "asdfs")
 
 		visit items_path
 		click_link "Add to Cart"
@@ -13,7 +13,7 @@ describe "User visits /cart with items in the cart." do
 
 		expect(page).to have_content(2)
 
+		expect(page).to have_content(3)
 	end
-	it "they can decrease the quantity" do
-	end 
+
 end 
