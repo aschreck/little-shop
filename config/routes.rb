60f4ed50a2f9203f87b95fc194cca7c3a7bfa466
randy-darsh <rspringer47@gmail.com>
2017-10-31 10:46:18 -0600

    add cart show page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-	resource :cart, only: [:show]
+	resource :cart, only: [:show, :create]
   get "/:title", to: "category#show"
 end
