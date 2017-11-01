b2640c9fec83b5c439a69122a8c7f0c16dd8d68d
Miguel Silva <msilva0897@gmail.com>
2017-10-31 21:08:01 -0600

    Story 11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      render :new
+    end
   end
 
   def destroy
