f7601a75ced79633386db128c1527e69b83c834a
randy-darsh <rspringer47@gmail.com>
2017-11-01 13:31:33 -0600

    changing category controller to categories controller

--- spec/features/user_does_not_see_checkout_spec.rb
+++ spec/features/user_does_not_see_checkout_spec.rb
@@ -31,7 +31,7 @@ describe "when user is not logged in he cannot see checkout" do
     expect(page).to have_content("Logout")
 
     click_on "View Cart"
-    save_and_open_page
+
     expect(page).to have_content("Checkout")
     expect(page).to have_content(2)
     expect(page).to have_content(3)
