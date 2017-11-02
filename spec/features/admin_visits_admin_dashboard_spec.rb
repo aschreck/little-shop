9cb4b312cfb1de71c50f93ac392325c5edfb1b0f
randy-darsh <rspringer47@gmail.com>
2017-11-02 13:00:53 -0600

    passing test

--- spec/features/admin_visits_admin_dashboard_spec.rb
+++ spec/features/admin_visits_admin_dashboard_spec.rb
@@ -2,9 +2,14 @@ require "rails_helper"
 describe "when admin can visit admin dashboard" do
   it "they see the admin dashboard page" do
     user = User.create(name: "Bob", username: "bobby", password: "1234", role: "admin")
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    visit admin_dashboard_path
+    visit login_path
+
+    fill_in "session[username]", with: user.username
+    fill_in "session[password]", with: user.password
+    click_on "Log In"
+
+    expect(current_path).to eq(admin_dashboard_path)
 
     expect(page).to have_content("Admin Dashboard")
   end
