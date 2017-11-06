e71d3a8459649763d05b2616382453868ac34b8d
Aaron Schreck <schreck.aaron@gmail.com>
2017-11-06 11:02:46 -0700

    Add requires for will_paginate.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
+require "will_paginate"
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
-    #@items = Item.paginate(page: params[:page], per_page: 30)
+    #@items = Item.all
+    @items = Item.paginate(page: params[:page], per_page: 30)
   end
 end
