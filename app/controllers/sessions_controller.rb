fbb2310d2c5ceb122a8dc8eb6a22129be138a6be
Aaron Schreck <schreck.aaron@gmail.com>
2017-11-07 19:29:16 -0700

    authorization so far

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,45 @@
 class SessionsController < ApplicationController
-  def new
+    protect_from_forgery except: [:create] 
+
+    def new
 
   end
 
   def create
-    @user = User.find_by(username: params[:session][:username])
-    if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      if current_user.admin?
-        redirect_to admin_dashboard_path
+    if request.env['omniauth.auth']
+      user = User.find_or_create_by(uid: request.env['omniauth.auth']["uid"])
+      user.provider = request.env["omniauth.auth"]["provider"]
+      user.uid = request.env["omniauth.auth"]["uid"]
+      user.name = request.env['omniauth.auth']["info"]["name"]
+      user.oauth_token = request.env['omniauth.auth']["credentials"]["token"]
+      user.oath_expires_at = Time.at(request.env['omniauth.auth']["credentials"]["expires_at"])
+      user.save!
+      user.password = "password"
+      session[:user_id] = user.id
+      redirect_to dashboard_path
+    else 
+      @user = User.find_by(username: params[:session][:username])
+      if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+        if current_user.admin?
+          redirect_to admin_dashboard_path
+        else
+          redirect_to dashboard_path
+        end
       else
-        redirect_to dashboard_path
+        render :new
       end
-    else
-      render :new
-    end
+    end 
   end
 
   def destroy
     session.clear
     redirect_to root_path
   end
+
+  def auth_create
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_path
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,18 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :edit, :update]
   resources :orders, only: [:index, :show, :create]
   resources :categories, only: [:index]
-  get    '/dashboard',     to: "base#dashboard"
-  get    '/login',         to: 'sessions#new'
-  post   '/login',         to: 'sessions#create'
-  delete '/logout',        to: 'sessions#destroy'
-	patch  '/cart/add',      to: "carts#add"
-	patch	 '/cart/subtract', to: "carts#subtract"
+  get    '/dashboard',          to: "base#dashboard"
+  get    '/login',              to: 'sessions#new'
+  post   '/login',              to: 'sessions#create'
+  delete '/logout',             to: 'sessions#destroy'
+	patch  '/cart/add',           to: "carts#add"
+	patch	 '/cart/subtract',      to: "carts#subtract"
+  get    "/auth/google_oauth2/callback", to: "sessions#create"
+  
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+  
+  
+  root to: "home#show"
   get    '/:title',        to: 'categories#show'
 end
